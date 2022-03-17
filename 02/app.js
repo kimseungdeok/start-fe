8ef82e4bc9a21a28dcb603509b1899f6bc54e2e0
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-17 17:04:11 +0900

    edit app.js

--- 02/app.js
+++ 02/app.js
@@ -40,3 +40,43 @@ person.sayHello();
 
 month = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
 
+var article = {
+    title: '제목1',
+    url: 'http://naver.com'
+}
+
+var articles = [article];
+// Create
+var todos = ['운동'];
+var todo = '게임';
+todos.push(todo);
+console.log(todos);
+// Read
+todos.forEach(function(todo){
+    console.log(todo);
+});
+// Update
+var todos = ['운동', '게임'];
+var updateTodo = '게임';
+var updateIndex = todos.findIndex(function(todo) {
+  return todo === updateTodo;
+});
+todos[updateIndex] = '공부';
+console.log(todos);
+
+// Delete 1
+var todos = ['운동', '공부', '목욕'];
+var deleteTodo = '공부';
+var deleteIndex = todos.findIndex(function(todo) {
+  return todo === deleteTodo;
+});
+todos.splice(deleteIndex, 1);
+console.log(todos);
+
+// Delete 2
+var todos = ['운동', '공부'];
+var deleteTodo = '공부';
+var newTodos = todos.filter(function(toods) {
+  return todo !== delteTodo;
+});
+console.log(newTodo);
