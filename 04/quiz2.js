46515d2dbd13f00a492722a81ae7f8b29526ec35
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-28 17:28:28 +0900

    quiz 2

--- 04/quiz2.js
+++ 04/quiz2.js
@@ -1,7 +1,38 @@
-var board = document.getElementById("board");
-for(var i=0; i<4; i++){
-    for(var j=0; j<4; j++){
-        // TODO : 여기에 필요한 코드를 작성해주세요.
-        
+var $board = document.getElementById("board");
+var boardCount = 16;
+var isWhite = false;
+var className = 'black'
+var boardLineCount = 4;
+
+var str = '';
+for(var i=0; i<boardCount; i++){
+    className = (isWhite) ? 'black': 'white';
+     
+    str += '<span class="'+className+'"></span>'
+
+    isWhite = !isWhite;
+
+    if(i % boardLineCount === 3) {
+        isWhite = !isWhite;
     }
+}
+
+$board.innerHTML = str;
+
+var $boards = document.querySelectorAll('span');
+var boardLength = $boards.length;
+var $select = null;
+
+function select(event) {
+    if($select) {
+        $select.className = $select.className.replace(' select', '')
+    }
+    var el = event.currentTarget;//현재 element가 나옴
+    console.log(el);
+    el.className += ' select';
+    $select = el;
+}
+
+for(var i = 0; i < boardLength; i++) {
+    $boards[i].addEventListener('click', select);
 }
