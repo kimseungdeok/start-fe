06b092d548a7d2ae722ccd69c168b4e5dcb83c4b
KimSeungDeok <tmdejr1117@gmail.com>
2022-04-14 11:02:35 +0900

    내용 수정

--- 06/quiz7.js
+++ 06/quiz7.js
@@ -1,24 +1,21 @@
 const $bug = document.getElementById('bug');
 const $box = document.querySelector('.box');
-let point = document.getElementById('point');
-let life = document.getElementById('life');
-let count = 1;
-let lifeCount = 9;
-
-console.log(document.getElementById('point').innerText)
+const $point = document.getElementById('point');
+const $life = document.getElementById('life');
+let count = 1; // 점수
+let lifeCount = 9; // 생명
 
 $box.addEventListener('click', (e) =>{
     if(lifeCount < 0) {
         alert('gameover');
     } else {
-    life.innerHTML = lifeCount--;
-    console.log('click', count, EventTarget);
+    $life.innerHTML = lifeCount--;
     $bug.style.left = `${Math.random()*400}px`;
     $bug.style.top = `${Math.random()*400}px`;
     }
-
 })
+
 $bug.addEventListener('click', (e) => {
-    point.innerHTML = count++;
-    life.innerHTML = lifeCount++;
+    $point.innerHTML = count++;
+    $life.innerHTML = lifeCount++;
 });
