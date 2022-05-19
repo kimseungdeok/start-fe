ba269d42e61ea3a77824ce82cee502172791f410
KimSeungDeok <tmdejr1117@gmail.com>
2022-05-19 17:43:58 +0900

    feat : npm 사용

--- 12/src/index.js
+++ 12/src/index.js
@@ -1,7 +1,19 @@
-// main.js
-import sumModule from './sum.js';
-import { sum2 } from './sum.js';
+// // main.js
+// import sumModule from './sum.js';
+// import { sum2 } from './sum.js';
 
-console.log(sumModule(1, 2));
+// console.log(sumModule(1, 2));
 
-console.log(sum2(2,3));
+// console.log(sum2(2,3));
+
+const queryString = require('query-string');
+
+console.log(location.search);
+//=> '?foo=bar'
+
+const parsed = queryString.parse(location.search);
+console.log(parsed);
+//=> {foo: 'bar'}
+
+console.log(location.hash);
+//=> '#token=bada55cafe'
