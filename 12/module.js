166e5d4a63b78d95346870311a876d41609c81a6
KimSeungDeok <tmdejr1117@gmail.com>
2022-05-27 23:58:14 +0900

    finish : 12주차

--- 12/module.js
+++ 12/module.js
@@ -1,12 +1,32 @@
 import { avg } from "./quiz12.js";
 import { random } from "./quiz12.js"
 
-function init() {
-    const avgResult = avg(1,2);
-    const randomResult = random(100);
+const avgBtn = document.querySelector('#avgBtn');
+const randBtn = document.querySelector('#randBtn');
 
-    console.log(avgResult);
-    console.log(randomResult);
-}
+// let avgValue = document.querySelector('#avg').value;
+let randValue = document.querySelector('#random').value;
 
-init();
+avgBtn.addEventListener('click', ()=> {
+    let avgValue = document.querySelector('#avg').value;
+    console.log(avg(...avgValue));
+    console.log(avgValue);
+    console.log();
+})
+
+randBtn.addEventListener('click', ()=> {
+    random(randValue);
+})
+
+
+// function init() {
+//     const avgResult = avg(1,2);
+//     const randomResult = random(100);
+
+//     console.log(avgResult);
+//     console.log(randomResult);
+// }
+
+// init();
+
+
